4b6949638f85b5a5a8477f232d9f187e99c4ea33
Anthony A <antja.03@gmail.com>
2022-07-25 20:16:52 -0400

    ActionRow allows undefined values in component array
    ActionRow:new and ActionRow:components now allows the provided component array to include undefined values which are skipped over. This is useful for adding/removing components based on a condition without having to ditch the builder pattern.

--- src/struct/discord/components/builder/ActionRow.ts
+++ src/struct/discord/components/builder/ActionRow.ts
@@ -9,14 +9,13 @@ export class ActionRow<T extends Component<ApiComponent<ComponentType>> = Compon
         super(ComponentType.ACTION_ROW);
     }
 
-    public static new<A extends Component<ApiComponent<ComponentType>>>(...components: A[]): ActionRow<A> {
+    public static new<A extends Component<ApiComponent<ComponentType>>>(...components: (A | undefined)[]): ActionRow<A> {
         return new ActionRow<A>().components(...components);
     }
 
-    public components(...components: T[]): ActionRow<T> {
+    public components(...components: (T | undefined)[]): ActionRow<T> {
         if (!this.data.components) this.data.components = [];
-
-        this.data.components.push(...components.map((component) => component.toJSON()));
+        this.data.components.push(...components.filter(component => component != undefined).map((component) => component!.toJSON()));
         return this;
     }
 }
